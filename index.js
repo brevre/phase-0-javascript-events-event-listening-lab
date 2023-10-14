b24ee6c9e6bfab96e2ea589c10d19f67fdac93c8
brevre <brendamakena09@gmail.com>
2023-10-14 02:47:01 +0200

    completed

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 function addingEventListener() {
+  // Find the element with the ID "input"
+  const input = document.getElementById('input');
+
+  // Create a function to handle the click event
+  function clickAlert() {
+    alert('I was clicked!');
+  }
+
+  // Add a click event listener to the "input" element, and call the clickAlert function
+  input.addEventListener('click', clickAlert);
 }
+
+addingEventListener(); // Call the function to set up the event listener
+
